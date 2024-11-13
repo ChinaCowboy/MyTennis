5c5fed4eb3092f9e7f52fe0b1b120052001d92c9
Bing N <markbullnb@hotmail.com>
2024-11-13 15:55:07 +0800

    update the image by selection

--- image-detector/src/components/objectDetector/objectDetector.js
+++ image-detector/src/components/objectDetector/objectDetector.js
@@ -42,32 +42,60 @@ const SelectButton = styled.button`
 const HiddenFileInput = styled.input`
   display: none;
 `;
+const TargetImg = styled.img`
+  height: 100%;
+`;
 
-
-
-const onSelectImage = async (e) => {
-  
-};
 
 export function ObjectDetector(props) 
 {
   const fileInputRef = useRef();
   const imageRef = useRef();
   const [isLoading, setLoading] = useState(false);
+  const [imgData, setImgData] = useState(null);
 
   const openFilePicker = () => {
     if (fileInputRef.current) fileInputRef.current.click();
   };
 
+  const readImage = (file) => {
+    return new Promise((rs, rj) => {
+      const fileReader = new FileReader();
+      fileReader.onload = () => rs(fileReader.result);
+      fileReader.onerror = () => rj(fileReader.error);
+      fileReader.readAsDataURL(file);
+    });
+  };
+  
+  const onSelectImage = async (e) => {
+    setLoading(true);
+    const file = e.target.files[0];
+    const imgData = await readImage(file);
+    setImgData(imgData);
+    const imageElement = document.createElement("img");
+    imageElement.src = imgData;
+    imageElement.onload = async () => {
+      const imgSize = {
+        width: imageElement.width,
+        height: imageElement.height,
+      };
+      
+      setLoading(false);
+    };
+  };
+  
 return (
   <ObjectDetectorContainer>
       <DetectorContainer>
+      {imgData && <TargetImg src={imgData} ref={imageRef} />}
+
       </DetectorContainer>
       <HiddenFileInput
         type="file"
         ref={fileInputRef}
         onChange={onSelectImage}
       />
+
       <SelectButton onClick={openFilePicker}>
         {isLoading ? "Recognizing..." : "Select Image"}
       </SelectButton>
