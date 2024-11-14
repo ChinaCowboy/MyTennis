9f5e8e4372bc601e6b42ab16c020eb31210d65c6
Bing N <markbullnb@hotmail.com>
2024-11-14 11:18:14 +0800

    add tensorflow coco--ssd

--- image-detector/src/components/objectDetector/objectDetector.js
+++ image-detector/src/components/objectDetector/objectDetector.js
@@ -1,7 +1,9 @@
 
 import styled from "styled-components";
 import React, { useRef, useState } from "react";
-
+import "@tensorflow/tfjs-backend-cpu";
+//import "@tensorflow/tfjs-backend-webgl";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
 
 const ObjectDetectorContainer = styled.div`
   display: flex;
@@ -18,6 +20,28 @@ const DetectorContainer = styled.div`
   align-items: center;
   justify-content: center;
   position: relative;
+`;
+const TargetBox = styled.div`
+  position: absolute;
+
+  left: ${({ x }) => x + "px"};
+  top: ${({ y }) => y + "px"};
+  width: ${({ width }) => width + "px"};
+  height: ${({ height }) => height + "px"};
+
+  border: 4px solid #1ac71a;
+  background-color: transparent;
+  z-index: 20;
+
+  &::before {
+    content: "${({ classType, score }) => `${classType} ${score.toFixed(1)}%`}";
+    color: #1ac71a;
+    font-weight: 500;
+    font-size: 17px;
+    position: absolute;
+    top: -1.5em;
+    left: -5px;
+  }
 `;
 
 const SelectButton = styled.button`
@@ -49,13 +73,45 @@ const TargetImg = styled.img`
 
 export function ObjectDetector(props) 
 {
+
   const fileInputRef = useRef();
   const imageRef = useRef();
   const [isLoading, setLoading] = useState(false);
   const [imgData, setImgData] = useState(null);
-
+  const [predictions, setPredictions] = useState([]);
   const openFilePicker = () => {
     if (fileInputRef.current) fileInputRef.current.click();
+  };
+  
+  const isEmptyPredictions = !predictions || predictions.length === 0;
+
+  const normalizePredictions = (predictions, imgSize) => {
+    if (!predictions || !imgSize || !imageRef) return predictions || [];
+    return predictions.map((prediction) => {
+      const { bbox } = prediction;
+      const oldX = bbox[0];
+      const oldY = bbox[1];
+      const oldWidth = bbox[2];
+      const oldHeight = bbox[3];
+
+      const imgWidth = imageRef.current.width;
+      const imgHeight = imageRef.current.height;
+
+      const x = (oldX * imgWidth) / imgSize.width;
+      const y = (oldY * imgHeight) / imgSize.height;
+      const width = (oldWidth * imgWidth) / imgSize.width;
+      const height = (oldHeight * imgHeight) / imgSize.height;
+
+      return { ...prediction, bbox: [x, y, width, height] };
+    });
+  };
+
+  const detectObjectsOnImage = async (imageElement, imgSize) => {
+    const model = await cocoSsd.load({});
+    const predictions = await model.detect(imageElement, 6);
+    const normalizedPredictions = normalizePredictions(predictions, imgSize);
+    setPredictions(normalizedPredictions);
+    console.log("Predictions: ", predictions);
   };
 
   const readImage = (file) => {
@@ -79,7 +135,8 @@ export function ObjectDetector(props)
         width: imageElement.width,
         height: imageElement.height,
       };
-      
+      await detectObjectsOnImage(imageElement, imgSize);
+ 
       setLoading(false);
     };
   };
@@ -88,7 +145,18 @@ return (
   <ObjectDetectorContainer>
       <DetectorContainer>
       {imgData && <TargetImg src={imgData} ref={imageRef} />}
-
+      {!isEmptyPredictions &&
+          predictions.map((prediction, idx) => (
+            <TargetBox
+              key={idx}
+              x={prediction.bbox[0]}
+              y={prediction.bbox[1]}
+              width={prediction.bbox[2]}
+              height={prediction.bbox[3]}
+              classType={prediction.class}
+              score={prediction.score * 100}
+            />
+          ))}
       </DetectorContainer>
       <HiddenFileInput
         type="file"
