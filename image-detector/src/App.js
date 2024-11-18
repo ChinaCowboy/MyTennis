42ec7b86f7d9debcc6b4af31769a35e5141999a3
Bing N <markbullnb@hotmail.com>
2024-11-18 17:21:23 +0800

    add face

--- image-detector/src/App.js
+++ image-detector/src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from "styled-components";
+import * as faceapi from 'face-api.js';
 import { ObjectDetector } from "./components/objectDetector/objectDetector";
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+
 
 const AppContainer = styled.div`
   width: 100%;
@@ -16,8 +20,46 @@ const AppContainer = styled.div`
 
 
 function App() {
+  const videoRef = useRef(null);
+  const [modelsLoaded, setModelsLoaded] = useState(false);
+  const [detections, setDetections] = useState([]);
+  useEffect(() => {
+    // Load face-api.js models
+    const loadModels = async () => {
+      const MODEL_URL = process.env.PUBLIC_URL + '/models';
+      await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+      await faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL);
+      await faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL);
+      await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL);
+      setModelsLoaded(true);
+    };
+
+    loadModels();
+  }, []);
+
+  const handleVideoOnPlay = () => {
+    setInterval(async () => {
+      if (videoRef.current) {
+        const detections = await faceapi.detectAllFaces(
+          videoRef.current,
+          new faceapi.TinyFaceDetectorOptions()
+        ).withFaceLandmarks().withFaceExpressions();
+
+        setDetections(detections);
+      }
+    }, 100);
+  };
   return (
     <AppContainer>
+        {modelsLoaded ? (
+        <FaceRecognition
+          videoRef={videoRef}
+          handleVideoOnPlay={handleVideoOnPlay}
+          detections={detections}
+        />
+      ) : (
+        <p>Loading models...</p>
+      )}
       <ObjectDetector />
     </AppContainer>
     // <div className="App">
